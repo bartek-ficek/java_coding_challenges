2423dd95aab2e27a249ed8766957df41e349e4fa
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-10 18:56:59 +0100

    third and forth solution with Streams

--- src/main/java/pl/bartek_ficek/fizzbuzz/FizzBuzz.java
+++ src/main/java/pl/bartek_ficek/fizzbuzz/FizzBuzz.java
@@ -3,13 +3,15 @@ package pl.bartek_ficek.fizzbuzz;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
 
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
     public static void main(String[] args) {
 
-        printFizzBuzz2(100);
+        printFizzBuzz4(100);
     }
 
     public static void printFizzBuzz1(int n) {
@@ -38,5 +40,21 @@ public class FizzBuzz {
                 STDOUT.info("\nFizz");
             } else STDOUT.info("\n{}", i);
         }
+    }
+
+    public static void printFizzBuzz3(int n) {
+        IntStream.rangeClosed(1, n).mapToObj(
+                i -> i % 15 == 0 ? "FizzBuzz" :
+                        (i % 5 == 0) ? "Buzz" :
+                                (i % 3 == 0) ? "Fizz" : i)
+                .forEach(System.out::println);
+    }
+
+    public static void printFizzBuzz4(int n) {
+        IntStream.rangeClosed(0, 100).mapToObj(
+                i -> i % 3 == 0 ?
+                        (i % 5 == 0 ? "FizzBuzz" : "Fizz") :
+                        (i % 5 == 0 ? "Buzz" : i))
+                .forEach(System.out::println);
     }
 }
