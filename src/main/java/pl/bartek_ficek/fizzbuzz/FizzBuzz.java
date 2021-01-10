e2cbd8376386d657adeca685a3e7ad2fe7f74180
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-10 17:25:13 +0100

    solution with slf4j

--- src/main/java/pl/bartek_ficek/fizzbuzz/FizzBuzz.java
+++ src/main/java/pl/bartek_ficek/fizzbuzz/FizzBuzz.java
@@ -1,20 +1,25 @@
 package pl.bartek_ficek.fizzbuzz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FizzBuzz {
+
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
             if (i % 3==0 && i % 15 !=0) {
-                System.out.println("Fizz");
+                STDOUT.info("\nFizz");
             }
             else if (i % 5==0 && i % 15 !=0) {
-                System.out.println("Buzz");
+                STDOUT.info("\nBuzz");
             }
             else if (i % 15==0) {
-                System.out.println("FizzBuzz");
+                STDOUT.info("\nFizzBuzz");
             }
-            else System.out.println(i);
+            else STDOUT.info("\n{}",i);
         }
     }
 }
