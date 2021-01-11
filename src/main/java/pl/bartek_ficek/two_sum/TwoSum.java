2e5857192ecd6abbdce3043355358147af7f026b
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-11 19:58:25 +0100

    two sum challenge initialized

---
+++ src/main/java/pl/bartek_ficek/two_sum/TwoSum.java
@@ -0,0 +1,13 @@
+package pl.bartek_ficek.two_sum;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TwoSum {
+
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+
+    public static void main(String[] args) {
+
+    }
+}
