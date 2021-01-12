a1024ca6e8f6b7f5f098c7481c0f9458287689d6
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-12 20:34:35 +0100

    Reverse a String - first solution with StringBuilder

--- src/main/java/pl/bartek_ficek/reverse_a_string/ReverseAString.java
+++ src/main/java/pl/bartek_ficek/reverse_a_string/ReverseAString.java
@@ -1,6 +1,22 @@
 package pl.bartek_ficek.reverse_a_string;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ReverseAString {
 
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
+    public static void main(String[] args) {
+
+        String givenString = "example";
+        String reversedString1 = reverseStringWay1(givenString);
+        STDOUT.info(reversedString1);
+    }
+
+    static String reverseStringWay1(String string) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string);
+        return stringBuilder.reverse().toString();
+    }
 }
