6b62d8071d0eb0299a3a929c53d2cacab6669333
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-13 15:54:16 +0100

    Reverse an Integer - challenge initialized

---
+++ src/main/java/pl/bartek_ficek/reverse_an_integer/ReverseInteger.java
@@ -0,0 +1,10 @@
+package pl.bartek_ficek.reverse_an_integer;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReverseInteger {
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+
+}
