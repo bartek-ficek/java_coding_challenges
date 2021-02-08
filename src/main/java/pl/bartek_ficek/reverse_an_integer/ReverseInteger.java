b11433c2a722f05abb0c5a1e33d89fa94cce98fd
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-08 22:13:08 +0100

    Reverse an Integer - first (mathematic) approach

--- src/main/java/pl/bartek_ficek/reverse_an_integer/ReverseInteger.java
+++ src/main/java/pl/bartek_ficek/reverse_an_integer/ReverseInteger.java
@@ -1,10 +1,19 @@
 package pl.bartek_ficek.reverse_an_integer;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReverseInteger {
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
+    public static void main(String[] args) {
+    }
+
+    public int reverseInteger(int input) {
+        long reversed = 0;
+        while (input != 0) {
+            reversed = reversed*10 + input % 10;
+            input /= 10;
+        }
+    }
 }
