2ed4889809b524632f3474ab93a4ccad9adcf5ec
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-08 22:31:41 +0100

    Reverse an Integer - mathematic approach implemented correctly

--- src/main/java/pl/bartek_ficek/reverse_an_integer/ReverseInteger.java
+++ src/main/java/pl/bartek_ficek/reverse_an_integer/ReverseInteger.java
@@ -7,13 +7,23 @@ public class ReverseInteger {
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
     public static void main(String[] args) {
+
+        ReverseInteger reverseInteger = new ReverseInteger();
+        int result = reverseInteger.reverseInteger(156);
+        STDOUT.info("{}",result);
+
     }
 
     public int reverseInteger(int input) {
         long reversed = 0;
         while (input != 0) {
-            reversed = reversed*10 + input % 10;
+            reversed = reversed * 10 + input % 10;
             input /= 10;
+
+            if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+                return 0;
+            }
         }
+        return (int) reversed;
     }
 }
